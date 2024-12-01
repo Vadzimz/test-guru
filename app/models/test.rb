8ee4b309213f6380c577f7403e76a218c8768def
Vadim Zotin <vadzimzotsin@gmail.com>
2024-12-01 16:53:19 +0300

    Add associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,10 @@
 class Test < ApplicationRecord
- 
-belongs_to :category
+  
+  has_many :questions
+  has_many :test_passages
+  has_many :users, through: :test_passages
+
+  belongs_to :category
 
   class << self
     def tests_list_by_category(category_title)
