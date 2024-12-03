c7407575519a0420c86988a1f591d102fb958b91
Vadim Zotin <vadzimzotsin@gmail.com>
2024-12-03 21:10:15 +0300

    Correct associations v2

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,7 +5,7 @@ class Test < ApplicationRecord
   has_many :users, through: :test_passages
 
   belongs_to :category
-  belongs_to :user, foreign_key: "author_id"
+  belongs_to :author, class_name: "User", foreign_key: "author_id", inverse_of: :own_tests 
 
   class << self
     def tests_list_by_category(category_title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   
   has_many :test_passages
   has_many :tests, through: :test_passages
-  has_many :tests, foreign_key: "author_id"
+  has_many :own_tests, class_name: "Test", foreign_key: "author_id", inverse_of: :author
 
   def tests_list_by_level(level)
     TestPassage.joins(:user, :test)
