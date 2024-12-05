094a6704576e8634d05c2ea9bac3e230ad758a58
Vadim Zotin <vadzimzotsin@gmail.com>
2024-12-05 19:47:03 +0300

    add scopes and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,18 @@ class User < ApplicationRecord
   has_many :tests, through: :test_passages
   has_many :own_tests, class_name: "Test", foreign_key: "author_id", inverse_of: :author
 
+  validates :email, presence: true
+
+  #Создайте инстанс-метод в модели User, который принимает в качестве аргумента значение уровня сложности и возвращает список всех Тестов, 
+  #которые проходит или когда-либо проходил Пользователь на этом уровне сложности
+
   def tests_list_by_level(level)
-    TestPassage.joins(:user, :test)
-      .where(test: {level: level}, user: {id: self.id})
-      .pluck(:title)
+    
+    #with join
+    #TestPassage.joins(:user, :test).where(test: {level: level}, user: {id: self.id}).pluck(:title)
+    
+    #with association
+    self.tests.where(level: level).pluck(:title)
   end
 
 end
