48e4d42d100180aebe84a308792674776aa665cb
Vadim Zotin <vadzimzotsin@gmail.com>
2024-12-23 15:11:14 +0300

    correct questions controller data

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,44 +1,41 @@
 class QuestionsController < ApplicationController
 
-  before_action :find_test, only: [:show, :destroy]
+  before_action :find_test, only: [:index, :new, :create]
+  before_action :find_question, only: [:show, :destroy]
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
   
   def index
-    @questions = Test.find(params[:test_id]).questions
-    @test = Test.find(params[:test_id])
-    #render plain: @questions.pluck(:body).join("\n")
+    @questions = @test.questions
   end
 
   def show
-    @question = Question.find(params[:id])
-    #render inline: "<%= @test.title %>"
   end
 
-
   def new
-    @question = Question.new
   end
 
   def create
-    @question = Question.new(question_params)
+    @question = @test.questions.new(question_params)
     redirect_to @question if @question.save
   end
 
   def destroy
-    @question = Question.find(params[:id])
     @question.destroy
-    redirect_to test_questions_path(@test), status: :see_other
+    redirect_to test_questions_path(@question.test), status: :see_other
   end
 
   private
 
   def find_test
-    test_id = Question.find(params[:id]).test_id
-    @test = Test.find_by(id: test_id)
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 
   def question_params
-    params.require(:question).permit(:body, :test_id)
+    params.require(:question).permit(:body)
   end
 
   def rescue_with_question_not_found
